2e95b40e0353fa0e95adcaa0a2b3e60028825697
unknown <choufackjennybrandy@gmail.com>
2023-12-06 16:52:39 +0100

    first commit

---
+++ src/main/java/com/example/firstwebappgrp2/connect/ConnectToList.java
@@ -0,0 +1,8 @@
+package com.example.firstwebappgrp2.connect;
+
+import com.example.firstwebappgrp2.service.LivreService;
+
+public class ConnectToList {
+
+    public static final LivreService livreService = new LivreService();
+}
